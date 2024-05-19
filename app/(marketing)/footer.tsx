767602403c5d43ac91864ad36a058804fb06dc9f
FlipThatPancake <pawelpedziwiatr@gmail.com>
2024-05-20 00:19:23 +0800

    sidebar

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
 export const Footer = () => {
     return (
         <footer className="hidden lg:block h-20 w-full border-b-2 border-slate-200 p-2">
-            Footer
+
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src="/United States.svg" alt="United States" width={32} height={40} className="mr-4 rounded-md" />
+                    English
+                </Button>
+            </div>
+
         </footer>
     )
 }
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -6,28 +6,36 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <div>
-      {/* <Button size="lg" variant="premium">Hello</Button> */}
+    <>
+
+      {/* Hero Section */}
       <div className="max-w-[998px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+
+        {/* Hero picture */}
         <div className="relative w-[240px] h-[240px] lg:w-[320px] lg:h-[320px] mb-8 lg:mb-0 lg:mr-8">
           <Image src="/friends.svg" alt="friends" fill />
         </div>
+
+        {/* Hero text */}
         <div className="flex flex-col items-center gap-y-8">
           <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
             Learn, practice, and master new languages with Lingo.
           </h1>
-          <div>
+
+          {/* Hero buttons */}
+          <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+            {/* Clerk loading animation */}
             <ClerkLoading>
               <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
             </ClerkLoading>
+
+            {/* If Clerk loaded */}
             <ClerkLoaded>
 
-
-              {/* User is Signed out */}
+              {/* If user is Signed out */}
               <SignedOut>
-
                 <SignUpButton mode="modal">
-                <Button size="lg" variant="secondary" className="w-full mb-2">
+                <Button size="lg" variant="secondary" className="w-full">
                   Get Started
                   </Button>
                 </SignUpButton>
@@ -37,10 +45,9 @@ export default function Home() {
                   I already have an account
                   </Button>
                 </SignInButton>
-
               </SignedOut>
 
-              {/* User is Signed in */}
+              {/* If user is Signed in */}
               <SignedIn>
                 <Button size="lg" variant="secondary" className="w-full" asChild>
                   <Link href="/learn">
@@ -50,8 +57,9 @@ export default function Home() {
               </SignedIn>
             </ClerkLoaded>
           </div>
+
         </div>
       </div>
-    </div>
+    </>
   )
 }
