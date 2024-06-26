cfd5782d2978a89aae0f824db0b2bfa78ea6c945
FlipThatPancake <pawelpedziwiatr@gmail.com>
2024-06-26 22:19:19 +0800

    june 26

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,9 +2,24 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
+import { getUserProgress } from "@/db/queries";
+import { redirect } from "next/navigation";
 
 
-const LearnPage = () => {
+const LearnPage = async () => {
+    const userProgressData = getUserProgress();
+
+    const [
+        userProgress
+    ] = await Promise.all([
+        userProgressData
+    ])
+
+    // Upon clicking "continue learning", immediately redirects to the courses page
+    if (!userProgressData || !userProgressData.activeCourse) {
+        redirect("/courses")
+    }
+
     return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,48 @@
 import { cache } from "react";
 import db from "./drizzle";
+import { auth } from "@clerk/nextjs/server";
+import { courses, userProgress } from "@/db/schema";
+import { eq } from "drizzle-orm";
+
+interface UserProgressData {
+    userId: string;
+    userName: string;
+    userImageSrc: string;
+    activeCourseId: number | null;
+    hearts: number;
+    points: number;
+    activeCourse: { id: number; title: string; imageSrc: string; } | null;
+}
+
+export const getUserProgress = cache(async () => {
+    const { userId } = await auth();
+
+    if (!userId) {
+        return null;
+    }
+
+    const data = await db.query.userProgress.findMany({
+        where: eq(userProgress.userId, userId),
+        with: {
+            activeCourse: true,
+        }
+        },
+    );
+
+    return data
+})
+
 
 export const getCourses = cache(async () => {
     const data = await db.query.courses.findMany();
     return data;
 })
+
+export const getCourseById = cache(async (courseId: number) => {
+    const data = await db.query.courses.findFirst({
+        where: eq(courses.id, courseId),
+        // TODO: Populate units and lessons
+    });
+
+    return data;
+});
