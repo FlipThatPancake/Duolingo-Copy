abf7e5132b3dc5825796fa36ff51eed6959781e6
Paul <pawel.pedziwiatr@cn.midea.com>
2024-08-08 20:47:45 +0800

    trip

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -4,6 +4,7 @@ import { challengeOptions, challenges } from "@/db/schema";
 import { useState } from "react";
 import { Header } from "./header";
 import { QuestionBubble } from "./question-bubble";
+import { Challenge } from "./challenge";
 
 type Props = {
     initialPercentage: number;
@@ -32,6 +33,7 @@ export const Quiz = ({
     });
 
     const challenge = challenges[activeIndex];
+    const options = challenge?.challengeOptions ?? [];
     const title = challenge.type === "ASSIST" ? "Select the correct meaning" : challenge.question;
 
     return (
@@ -51,6 +53,13 @@ export const Quiz = ({
                             {challenge.type === "ASSIST" && (
                                 <QuestionBubble question={challenge.question} />
                             )}
+                            <Challenge 
+                                options={options} 
+                                onSelect={() => {}} 
+                                status="none"
+                                selectedOption={undefined} 
+                                disabled={false} 
+                                type={challenge.type}/>
                         </div>
                     </div>
                 </div>
